3c94f5b007e418b29561245e62eb7bbedcfc7760
Tiedapang <2750428832@qq.com>
2020-12-09 17:17:42 +0800

    [thj] feat: 获取和重置所有过滤器模型

--- src/app/components/ag-grid-column-filter-demo/ag-grid-column-filter-demo.component.ts
+++ src/app/components/ag-grid-column-filter-demo/ag-grid-column-filter-demo.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ColumnApi, GridApi} from 'ag-grid-community';
 import {GridDataServiceService} from '../service/grid-data-service.service';
 
+
 @Component({
     selector: 'app-ag-grid-column-filter-demo',
     templateUrl: './ag-grid-column-filter-demo.component.html',
@@ -16,21 +17,34 @@ export class AgGridColumnFilterDemoComponent implements OnInit {
 
     constructor(private gridDataService: GridDataServiceService) {
         this.columnDefs = [
-            {field: 'athlete'},
+            {   field: 'athlete',
+                filter: 'agTextColumnFilter',
+                filterParams: {button: ['reset', 'apply']}
+            },
             {
                 field: 'age',
                 filter: 'agNumberColumnFilter',
+                filterParams: {button: ['reset', 'apply'], closeOnApply: true},
                 maxWidth: 100,
             },
-            {field: 'country'},
+            {   field: 'country',
+                filter: 'agSetColumnFilter',
+                filterParams: {
+                    buttons: ['clear', 'apply'],
+                },
+            },
             {
                 field: 'year',
                 maxWidth: 100,
+                filter: 'agSetColumnFilter',
+                filterParams: {
+                    buttons: ['apply', 'cancel'],
+                    closeOnApply: true,
+                },
             },
             {
                 field: 'date',
                 filter: 'agDateColumnFilter',
-                // filterParams: filterParams,
             },
             {field: 'sport'},
             {
@@ -54,6 +68,7 @@ export class AgGridColumnFilterDemoComponent implements OnInit {
             flex: 1,
             minWidth: 150,
             filter: true,
+            sortable: true
         };
     }
 
@@ -66,5 +81,44 @@ export class AgGridColumnFilterDemoComponent implements OnInit {
         this.gridDataService.getBigNumbersOfRowDatas().subscribe((data) => {
             this.rowData = data;
         });
+    }
+
+    saveFilterModel(): void{
+        const keys = Object.keys(this.gridApi.getFilterModel());
+        const saveFilters = keys.length > 0 ? keys.join(', ') : '(none)';
+        document.querySelector('#savedFilters').innerHTML = saveFilters;
+    }
+
+    restoreFilterModel(): void{
+        this.gridApi.setFilterModel(this.gridApi.getFilterModel());
+    }
+
+    restoreFromHardCoded(): void{
+        const hardcodedFilter = {
+            country: {
+                type: 'set',
+                values: ['Ireland', 'United States'],
+            },
+            age: {
+                type: 'lessThan',
+                filter: '30',
+            },
+            athlete: {
+                type: 'startsWith',
+                filter: 'Mich',
+            },
+            date: {
+                type: 'lessThan',
+                dateFrom: '2010-01-01',
+            },
+        };
+        this.gridApi.setFilterModel(hardcodedFilter);
+    }
+
+    clearFilters(): void{
+        this.gridApi.setFilterModel(null);
+    }
+
+    destroyFilter(): void{
     }
 }
